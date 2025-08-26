a19e07b3d780b1f9a5deb41e489108bf27343bda
Mohd Rayees <168510920+iam-rayees@users.noreply.github.com>
2025-08-26 23:05:52 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker pull rayeez/walmart-python-flask-app:latest
 
-docker run --rm -d -p 5000:5000 rayeez/walmart-python-flask-app:latest
+docker run -d -p 5000:5000 rayeez/walmart-python-flask-app:latest
