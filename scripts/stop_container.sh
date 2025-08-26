42c046f6cbef23f5249cf533e034771020c6306f
Mohd Rayees <168510920+iam-rayees@users.noreply.github.com>
2025-08-26 22:26:36 +0530

    entered new script for stopping containers

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-docker stop $(docker ps -aq)
+containers=$(docker ps -aq)
 
+if [ -n "$containers" ]; then
+  echo "Stopping containers: $containers"
+  docker stop $containers
+else
+  echo "No containers are running."
+fi
