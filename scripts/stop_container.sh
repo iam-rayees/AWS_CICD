b0579c3dea3847085de238f57e578503fcf40c90
Mohd Rayees <168510920+iam-rayees@users.noreply.github.com>
2025-08-26 22:51:07 +0530

    new message

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,11 +1,2 @@
 #!/bin/bash
-containerIds=$(docker ps -q)
-
-if [[ -z "$containerIds" ]]; then
-   echo "No running containers found."
-   exit 0
-else
-   echo "Stopping containers: $containerIds"
-   docker rm -f $containerIds
-   # Or, if you want to remove them completely:
-fi
+echo "Hi"
