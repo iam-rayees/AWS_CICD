ccec5c01c0eb55383223dca328b2ec254ed75b99
Mohd Rayees <168510920+iam-rayees@users.noreply.github.com>
2025-08-27 21:27:50 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 containerIds=$(docker ps -aq)
 
-if [[ -z "$containerIds" ]]; then
-   echo "No containers found."
-   exit 0
-else
+if [[ ! -z "$containerIds" ]]; then
    docker rm -f $containerIds
+else
+   exit 0
 fi
